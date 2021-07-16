965e873b06e62280d291b5edf229b0f22c18665a
Shilei Mao <shilei.mao@horizon-globex.ie>
2021-07-16 12:17:23 +0100

    Initial commit

---
+++ Example/MySDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MySDK
+//
+//  Created by ShileiMao on 07/16/2021.
+//  Copyright (c) 2021 ShileiMao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
